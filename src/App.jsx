3635ffb3827d93ebdbbbdd8b5a9ede89572ae73d
Prachi Gour <prachi.gour602@gmail.com>
2026-04-08 00:13:44 +0530

    remove unused code

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-// import Dashboard from './pages/Dashboard'
-import Home from './pages/Home'
 import VirtualList from './pages/VirtualList'
 
 function App() {
 
   return (
     <>
-      {/* <Home /> */}
       <VirtualList />
     </>
   )
